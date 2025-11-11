7b1ddb7aae37abf3e2b8368cb6aa0cf38d0164fe
Biagio Piraino <biagiopirainowork@gmail.com>
2025-11-11 21:14:31 +0000

    scrapers - add metadata and readmore content

--- scraper/internal/scrapers/yahoo.go
+++ scraper/internal/scrapers/yahoo.go
@@ -12,7 +12,9 @@ import (
 )
 
 const rootWebsite = "https://uk.finance.yahoo.com/news"
-const contentSelector = ".mainContainer .body-wrap .body .bodyItems-wrapper p"
+const metadataSelector = ".mainContainer .byline .byline-attr"
+const contentSelector = ".mainContainer .body-wrap .body .bodyItems-wrapper"
+const readmoreSelector = ".mainContainer .body-wrap .body .read-more-wrapper"
 
 type YahooScraper struct{}
 
@@ -23,14 +25,14 @@ func NewReutersScraper() *YahooScraper {
 func (s *YahooScraper) ScrapeWebsite() {
 	c := colly.NewCollector(
 		colly.Async(true),
-		colly.MaxDepth(10), // leave to 0 default in production to keep scraping the site
+		colly.MaxDepth(3), // leave to 0 default in production to keep scraping the site
 		colly.URLFilters(regexp.MustCompile("^"+regexp.QuoteMeta(rootWebsite))),
 		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"),
 	)
 
 	c.Limit(&colly.LimitRule{
 		DomainGlob:  "*uk.finance.yahoo*",
-		Parallelism: 2,
+		Parallelism: 5,
 		Delay:       2 * time.Second,
 		RandomDelay: 4 * time.Second,
 	})
@@ -41,16 +43,39 @@ func (s *YahooScraper) ScrapeWebsite() {
 		e.Request.Visit(link)
 	})
 
-	// extract content callback
+	// extract metadata callback
+	c.OnHTML(metadataSelector, func(e *colly.HTMLElement) {
+		requestId := utils.GetRequestIdFromElement(e)
+		logger.LogRequest(requestId, fmt.Sprintf("found metadata at %s", e.Request.URL))
+
+		author := e.ChildText(".byline-attr-author a")
+		datePublished := e.ChildText(".byline-attr-time-style .byline-attr-meta-time")
+		fmt.Printf("URL: %s\n", e.Request.URL.String())
+		fmt.Printf("Author: %s %s\n", author, e.Request.URL.String())
+		fmt.Printf("Published: %s\n", datePublished)
+		fmt.Println("---")
+	})
+
+	// extract main content
 	c.OnHTML(contentSelector, func(e *colly.HTMLElement) {
 		requestId := utils.GetRequestIdFromElement(e)
 		logger.LogRequest(requestId, fmt.Sprintf("found content at %s", e.Request.URL))
 
-		//paragraphText := e.Text
-		// // Clean up and print the extracted text
-		// fmt.Printf("URL: %s\n", e.Request.URL.String())
-		// fmt.Printf("Extracted Paragraph: %s\n", paragraphText)
-		// fmt.Println("---")
+		paragraphText := e.ChildText("p")
+		fmt.Printf("URL: %s\n", e.Request.URL.String())
+		fmt.Printf("Extracted Paragraph: %s\n", paragraphText)
+		fmt.Println("---")
+	})
+
+	// extract read more content
+	c.OnHTML(readmoreSelector, func(e *colly.HTMLElement) {
+		requestId := utils.GetRequestIdFromElement(e)
+		logger.LogRequest(requestId, fmt.Sprintf("found read more content at %s", e.Request.URL))
+
+		paragraphText := e.ChildText("p")
+		fmt.Printf("URL: %s\n", e.Request.URL.String())
+		fmt.Printf("Extracted Read More Paragraph: %s\n", paragraphText)
+		fmt.Println("---")
 	})
 
 	c.OnRequest(func(r *colly.Request) {
