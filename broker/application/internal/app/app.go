c9f62a42178bcd61b72d7f37d1c78e15c531109d
Biagio Piraino <biagiopirainowork@gmail.com>
2025-11-26 14:19:58 +0000

    app pattern applied to do broker

---
+++ broker/application/internal/app/app.go
@@ -0,0 +1,101 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/IBM/sarama"
+	"github.com/biagioPiraino/delphico/consumer/internal/consumers"
+	"github.com/biagioPiraino/delphico/consumer/internal/databases"
+)
+
+type Config struct {
+	BrokerAddress  string
+	ConsumerConfig *sarama.Config
+}
+
+type App struct {
+	config Config
+}
+
+func NewApp(config Config) *App {
+	return &App{
+		config: config,
+	}
+}
+
+func (a *App) Run() {
+	// initialise database
+	db, err := databases.InitialisePool()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	// happy path
+	defer func() {
+		cancel() // cancelling context and close database on exit
+		if err := db.Pool.Close(); err != nil {
+			log.Fatalf("%v", err)
+		} else {
+			fmt.Println("database closed correctly")
+		}
+	}()
+
+	// signals sentinel
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		<-sigchan
+		fmt.Println("captured signal. closing application")
+		cancel()
+	}()
+
+	var wg sync.WaitGroup
+	for i := 0; i < len("zzz"); i++ {
+		wg.Add(1)
+		go a.runFinanceConsumerGroup(db, &wg, ctx)
+	}
+
+	wg.Wait()
+	fmt.Println("all consumers are shut down")
+}
+
+func (a *App) runFinanceConsumerGroup(db *databases.DelphineDb, wg *sync.WaitGroup, ctx context.Context) {
+	defer wg.Done() // closing wait group on exit
+
+	consumer := consumers.NewFinanceConsumer(db)
+	client, err := sarama.NewConsumerGroup([]string{a.config.BrokerAddress}, consumer.GroupId, a.config.ConsumerConfig)
+	if err != nil {
+		fmt.Println("error initialising consumer")
+		return
+	}
+	defer func() {
+		if err := client.Close(); err != nil {
+			log.Println(fmt.Sprintf("error closing sarama consumer: %v", err))
+		} else {
+			fmt.Println("consumer closed correctly")
+		}
+	}()
+
+	for {
+		err := client.Consume(ctx, consumer.Topics, consumer)
+
+		if ctx.Err() != nil {
+			return
+		}
+
+		if err != nil {
+			log.Printf("Finance consumer error: %v", err)
+			// prevent rapid error triggering
+			time.Sleep(3 * time.Second)
+		}
+	}
+}
--- broker/application/main.go
+++ broker/application/main.go
@@ -1,98 +1,26 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"os"
-	"os/signal"
-	"sync"
-	"syscall"
 	"time"
 
 	"github.com/IBM/sarama"
-	"github.com/biagioPiraino/delphico/consumer/internal/consumers"
-	"github.com/biagioPiraino/delphico/consumer/internal/databases"
+	"github.com/biagioPiraino/delphico/consumer/internal/app"
 	"github.com/biagioPiraino/delphico/consumer/internal/utils"
 )
 
-var (
-	brokers []string
-	config  *sarama.Config
-)
-
-func init() {
-	// load env variables
+func main() {
 	utils.LoadEnvVariables()
 
-	// setup broker and consumers
-	brokers = []string{"localhost:9094"}
 	config := sarama.NewConfig()
 	config.Consumer.Offsets.Initial = sarama.OffsetOldest
 	config.Consumer.Offsets.AutoCommit.Enable = true
 	config.Consumer.Offsets.AutoCommit.Interval = 1 * time.Second
-}
 
-func runFinanceConsumerGroup(db *databases.DelphineDb, wg *sync.WaitGroup, ctx context.Context) {
-	defer wg.Done()
-
-	consumer := consumers.NewFinanceConsumer(db)
-	client, err := sarama.NewConsumerGroup(brokers, consumer.GroupId, config)
-	if err != nil {
-		log.Fatalf("Error while initialising finance consumer: %v", err)
-	}
-	defer func() {
-		if err := client.Close(); err != nil {
-			log.Println(fmt.Sprintf("error closing sarama consumer: %v", err))
-		}
-	}()
-
-	for {
-		err := client.Consume(ctx, consumer.Topics, consumer)
-
-		if ctx.Err() != nil {
-			// just return, delegate closing to cleanup function in consumer logic
-			return
-		}
-
-		if err != nil {
-			log.Printf("Finance consumer error: %v", err)
-			// prevent rapid error triggering
-			time.Sleep(3 * time.Second)
-		}
-	}
-}
-
-func main() {
-	// initialise database
-	db, err := databases.InitialisePool()
-	if err != nil {
-		panic(err)
+	appConfig := app.Config{
+		BrokerAddress:  "localhost:9094",
+		ConsumerConfig: config,
 	}
 
-	// closing database on defer
-	defer func() {
-		if err := db.Pool.Close(); err != nil {
-			log.Println(fmt.Sprintf("error closing database, %v", err))
-		} else {
-			log.Println("Database closed successfully")
-		}
-	}()
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go runFinanceConsumerGroup(db, &wg, ctx)
-
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
-
-	<-signals // graceful closure in case app terminated
-	log.Println("Received termination signals...closing")
-	cancel()
-
-	wg.Wait()
-	log.Println("All consumer are shut down")
+	application := app.NewApp(appConfig)
+	application.Run()
 }
